efc30a85e32e2c45c04810c22ff7924f737c2de3
mariamslu <mariamslu@yandex.ru>
2018-03-18 21:59:28 +0300

    Point Class added

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,16 +10,27 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '@';
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 4;
+            p1.sym = '*';
+            Draw(p1.x, p1.y, p1.sym);
 
-            int x2 = 3;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 6;
+            p2.sym = '#';
+            Draw(p2.x, p2.y, p2.sym);
 
+            //int x1 = 1;
+            //int y1 = 4;
+            //char sym1 = '*';
+            //Draw (x1, y1, sym1);
+
+            //int x2 = 4;
+            //int y2 = 6;
+            //char sym2 = '#';
+            //Draw (x2, y2, sym2);
 
             Console.ReadKey();
         }
